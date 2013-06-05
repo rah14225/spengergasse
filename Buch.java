f666509ff1a4e3bebebc6d65ea13dca2fbdc9ae0
Mahmudur Rahman <rah14225@spengergasse.at>
2013-06-05 22:56:56 +0200

    Initial version of my code in java

---
+++ Buch.java
@@ -0,0 +1,72 @@
+
+/**
+ * Write a description of class Buch here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Buch
+{
+    private boolean taschenbuch;
+    private int seiten;
+    private String titel;
+    private String autor;
+    public Buch()
+    {
+        taschenbuch=true;
+        seiten=200;
+        titel="Java für Anfänger";
+        autor="Max Schreiber";
+    }
+    public Buch(boolean taschenbuch, int seiten, String titel, String autor)
+    {
+        this.taschenbuch=taschenbuch;
+        this.seiten=seiten;
+        this.titel=titel;
+        this.autor=autor;
+    }
+    public void setTaschenbuch(boolean taschenbuch)
+    {
+        this.taschenbuch=taschenbuch;
+    }
+    public void setSeiten(int seiten)
+    {
+        this.seiten=seiten;
+    }
+    public void setTitel(String titel)
+    {
+        this.titel=titel;
+    }
+    public void setAutor(String autor)
+    {
+        this.autor=autor;
+    }
+    public boolean getTaschenbuch()
+    {
+        return taschenbuch;
+    }
+    public int getSeiten()
+    {
+        return seiten;
+    }
+    public String getTitel()
+    {
+        return titel;
+    }
+    public String getAutor()
+    {
+        return autor;
+    }
+}    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
+        
