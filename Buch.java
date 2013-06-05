292349649bbf785fc56eb11b1a7247154e19ee97
Mahmudur Rahman <rah14225@spengergasse.at>
2013-06-05 23:02:48 +0200

    Some changes

--- Buch.java
+++ Buch.java
@@ -2,8 +2,8 @@
 /**
  * Write a description of class Buch here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Mahmudur Rahman 
+ * @version 1.1
  */
 public class Buch
 {
